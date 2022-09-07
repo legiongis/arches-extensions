42f083b9396d64645d7c25188e95c04d93a5a738
Adam Cox <mr.adamcox@gmail.com>
2022-09-07 17:22:48 -0500

    update package name
    Signed-off-by: Adam Cox <mr.adamcox@gmail.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ArchesDevTools',
+    name='arches-dev-tools',
     version='0.1.0',
     author='Adam Cox',
     author_email='adam@legiongis.com',
