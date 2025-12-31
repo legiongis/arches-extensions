af76a0efb1e31c52626bc3412ea24892b750884e
Adam Cox <mr.adamcox@gmail.com>
2025-12-31 22:11:50 +0000

    better handle sync, add docstring

--- arches_extensions/management/commands/run_db_backup.py
+++ arches_extensions/management/commands/run_db_backup.py
@@ -11,7 +11,26 @@ from django.core.management.base import BaseCommand, CommandError
 from django.db.utils import IntegrityError
 
 class Command(BaseCommand):
-    """
+    """Run pg_dump to SQL file. Creates a local file and then uploads to S3.
+    This command assumes you have the aws cli tool installed and configured.
+
+    Follows this backup pattern:
+
+    - Daily backups for the last 10 days
+    - Backups on the 1st and 15th of each month forever
+
+    Daily backups are stored locally and rotated everyday. Directory structure:
+    LOCAL
+      .db_backups/daily/
+        <YYYYMMDD>__<db name>.sql
+        (repeated for the last 10 days)
+
+    S3
+      bucket_name/daily <- synced from local daily directory
+      YYYY/
+       all 1st and 15th of the month backups for one year
+      YYYY/
+       same for another year, etc.
     """
 
     def __init__(self, *args, **kwargs):
@@ -48,7 +67,7 @@ class Command(BaseCommand):
         now = datetime.now()
 
         backup_dir = Path(Path(settings.APP_ROOT).parent, ".db_backups", "daily")
-        backup_dir.mkdir(exist_ok=True)
+        backup_dir.mkdir(exist_ok=True, parents=True)
 
         db_name = settings.DATABASES['default']['NAME']
         db_user = settings.DATABASES['default']['USER']
@@ -63,32 +82,31 @@ class Command(BaseCommand):
             "pg_dump",
             "-U", db_user,
             "-h", db_host,
-            "-p", db_port,
+            "-p", str(db_port),
             "-f", str(fpath.resolve()),
             db_name,
         ]
 
         use_env = os.environ.copy()
         use_env['PGPASSWORD'] = db_pass
-        print(' '.join(cmd))
-        subprocess.Popen(cmd, env=use_env)
+        p = subprocess.Popen(cmd, env=use_env)
+        exit_code = p.wait()
 
         day, year = now.day, now.year
 
+        ## on the 1st and 15th of the month upload the dump to yearly archive
         if day in [1, 15]:
 
             cmd2 = [
                 "aws", "s3", "cp", fpath, f"s3://{bucket}/{year}/"
             ]
-
             cmd2 = apply_profile(cmd2)
-
-            subprocess.Popen(cmd2)
+            subprocess.run(cmd2)
 
         if not options["skip_rotate"]:
             ## cleanup any local files that are over 10 days old.
             ## sort all of the existing local daily files
-            local_dailies = sorted(list(backup_dir.glob("*.sql")))
+            local_dailies = sorted(list(backup_dir.glob("*")))
             limit = 10
             ## slice off any local dailies beyond the limit
             for path in local_dailies[:-limit]:
@@ -96,7 +114,7 @@ class Command(BaseCommand):
 
         if not options["skip_sync"]:
             cmd3 = [
-                "aws", "s3", "sync", str(backup_dir.resolve()), f"s3://{bucket}/daily/"
+                "aws", "s3", "sync", str(backup_dir.resolve()), f"s3://{bucket}/daily/", "--delete"
             ]
             cmd3 = apply_profile(cmd3)
-            subprocess.Popen(cmd3)
+            subprocess.run(cmd3)
